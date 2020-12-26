7cead6c41a39927a81c67750b70e429c70690bbd
River Lynn Bailey <riverlynnbailey@gmail.com>
2020-12-26 16:19:51 -0600

    moving everything into gitup-specs.sh file

--- gitup-specs.sh
+++ gitup-specs.sh
@@ -1,7 +1,70 @@
 #!/bin/sh
-source ./gitup-specs.config.sh
 
+# SCRIPT VARS TO USE
+# ------------------
+GITUP=". $PWD/gitup.sh"
+LOCAL_REPO='/tmp/gitup-test-repos/test-local-repo'
+REMOTE_REPO='/tmp/gitup-test-repos/test-remote-repo'
+MAIN_BRANCH='main'
+DEV_BRANCH='development'
 TEST_BRANCH='test-branch'
+
+# SPECS
+# -----
+
+testSkipAll() {
+  $GITUP -su -sa -sm
+}
+
+testRunAll() {
+  $GITUP -sa
+}
+
+# SETUP AND TEARDOWN
+# ------------------
+
+oneTimeSetUp() {
+  echo "GITUP SPECS: SETUP"
+  __setup_remote_repo
+  __setup_local_repo
+}
+
+oneTimeTearDown() {
+  echo "GITUP SPECS: TEARDOWN"
+  rm -rdf $REMOTE_REPO
+  rm -rdf $LOCAL_REPO
+}
+
+__setup_remote_repo() {
+  echo "GITUP SPECS: - create remote repo"
+  pushd $PWD
+    mkdir -p $REMOTE_REPO
+    cd $REMOTE_REPO
+
+    git init --bare
+  popd
+}
+
+__setup_local_repo() {
+  echo "GITUP SPECS: - create local repo"
+  git clone $REMOTE_REPO $LOCAL_REPO
+
+  pushd $PWD
+    cd $LOCAL_REPO
+
+    git checkout -b $MAIN_BRANCH
+    touch main_sample
+    git add .
+    git commit -m 'added sample commit'
+    git push origin $MAIN_BRANCH
+
+    git checkout -b $DEV_BRANCH
+    touch dev_sample
+    git add .
+    git commit -m 'added development commit'
+    git push origin $DEV_BRANCH
+  popd
+}
 
 setUp() {
   pushd $PWD
@@ -20,14 +83,6 @@ tearDown() {
   popd
   echo "-------"
   echo
-}
-
-testSkipAll() {
-  $GITUP -su -sa -sm
-}
-
-testRunAll() {
-  $GITUP
 }
 
 . ./shunit2/shunit2
