f1e83879aa2d50de1b264bfcde8f64c07ff783b5
River Lynn Bailey <riverlynnbailey@gmail.com>
2020-12-28 19:46:26 -0600

    replaced pushd/popd with

--- gitup-specs.sh
+++ gitup-specs.sh
@@ -16,7 +16,7 @@ TEST_BRANCH='test-branch'
 # -------------------
 
 test_run_full_gitup() {
-  pushd $PWD
+  echo $(
     cd $LOCAL_REPO
 
     $GITUP
@@ -24,11 +24,11 @@ test_run_full_gitup() {
     assertEquals 1 $mock_git_update_called
     assertEquals 1 $mock_install_dependencies_called
     assertEquals 1 $mock_migrations_called
-  popd
+  )
 }
 
 test_skip_all_with_cli() {
-  pushd $PWD
+  echo $(
     cd $LOCAL_REPO
 
     $GITUP -sa -su -sm
@@ -36,13 +36,11 @@ test_skip_all_with_cli() {
     assertEquals 0 $mock_git_update_called
     assertEquals 0 $mock_install_dependencies_called
     assertEquals 0 $mock_migrations_called
-  popd
+  )
 }
 
 test_skip_update_with_cli() {
-  local called=0
-
-  pushd $PWD
+  echo $(
     cd $LOCAL_REPO
 
     $GITUP -su
@@ -50,11 +48,11 @@ test_skip_update_with_cli() {
     assertEquals 0 $mock_git_update_called
     assertEquals 1 $mock_install_dependencies_called
     assertEquals 1 $mock_migrations_called
-  popd
+  )
 }
 
 test_skip_install_dependencies_with_cli() {
-  pushd $PWD
+  echo $(
     cd $LOCAL_REPO
 
     $GITUP -sa
@@ -62,11 +60,11 @@ test_skip_install_dependencies_with_cli() {
     assertEquals 1 $mock_git_update_called
     assertEquals 0 $mock_install_dependencies_called
     assertEquals 1 $mock_migrations_called
-  popd
+  )
 }
 
 test_skip_migrations_with_cli() {
-  pushd $PWD
+  echo $(
     cd $LOCAL_REPO
 
     $GITUP -sm
@@ -75,7 +73,7 @@ test_skip_migrations_with_cli() {
     assertEquals 1 $mock_git_update_called
     assertEquals 1 $mock_install_dependencies_called
     assertEquals 0 $mock_migrations_called
-  popd
+  )
 }
 
 # MOCK FUNCTIONS
@@ -123,31 +121,31 @@ oneTimeTearDown() {
 }
 
 __setup_remote_repo() {
-  pushd $PWD
+  echo $(
     mkdir -p $REMOTE_REPO
     cd $REMOTE_REPO
     git init --bare
-  popd
+  )
 }
 
 __setup_local_repo() {
-  pushd $PWD
+  echo $(
     git clone $REMOTE_REPO $LOCAL_REPO
     cd $LOCAL_REPO
 
     git checkout -b $MAIN_BRANCH
     echo .gituprc >> $LOCAL_REPO/.gitignore
-    touch main_sample
+    touch 'main_sample'
     git add .
     git commit -m 'added sample commit'
     git push origin $MAIN_BRANCH
 
     git checkout -b $DEV_BRANCH
-    touch dev_sample
+    touch 'dev_sample'
     git add .
     git commit -m 'added development commit'
     git push origin $DEV_BRANCH
-  popd
+  )
 }
 
-. ./shunit2/shunit2
+source ./shunit2/shunit2
