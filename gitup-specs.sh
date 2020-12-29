fa021c0f9c3ae743fc2193207d253f4fc24270fa
River Lynn Bailey <riverlynnbailey@gmail.com>
2020-12-28 19:51:00 -0600

    code formatting to make things clean

--- gitup-specs.sh
+++ gitup-specs.sh
@@ -1,6 +1,5 @@
-#! /bin/zsh -y
-SHUNIT_PARENT=$0
-# --------------
+#! /bin/sh
+# --------
 
 GITUP=". $PWD/gitup.sh"
 
@@ -15,66 +14,56 @@ TEST_BRANCH='test-branch'
 # SPECS TO SKIP STEPS
 # -------------------
 
-test_run_full_gitup() {
-  (
-    cd $LOCAL_REPO
+test_run_full_gitup() {(
+  cd $LOCAL_REPO
 
-    $GITUP
-    assertEquals 0 $?
-    assertEquals 1 $mock_git_update_called
-    assertEquals 1 $mock_install_dependencies_called
-    assertEquals 1 $mock_migrations_called
-  )
-}
+  $GITUP
+  assertEquals 0 $?
+  assertEquals 1 $mock_git_update_called
+  assertEquals 1 $mock_install_dependencies_called
+  assertEquals 1 $mock_migrations_called
+)}
 
-test_skip_all_with_cli() {
-  (
-    cd $LOCAL_REPO
+test_skip_all_with_cli() {(
+  cd $LOCAL_REPO
 
-    $GITUP -sa -su -sm
-    assertEquals 0 $?
-    assertEquals 0 $mock_git_update_called
-    assertEquals 0 $mock_install_dependencies_called
-    assertEquals 0 $mock_migrations_called
-  )
-}
+  $GITUP -sa -su -sm
+  assertEquals 0 $?
+  assertEquals 0 $mock_git_update_called
+  assertEquals 0 $mock_install_dependencies_called
+  assertEquals 0 $mock_migrations_called
+)}
 
-test_skip_update_with_cli() {
-  (
-    cd $LOCAL_REPO
+test_skip_update_with_cli() {(
+  cd $LOCAL_REPO
 
-    $GITUP -su
-    assertEquals 0 $?
-    assertEquals 0 $mock_git_update_called
-    assertEquals 1 $mock_install_dependencies_called
-    assertEquals 1 $mock_migrations_called
-  )
-}
+  $GITUP -su
+  assertEquals 0 $?
+  assertEquals 0 $mock_git_update_called
+  assertEquals 1 $mock_install_dependencies_called
+  assertEquals 1 $mock_migrations_called
+)}
 
-test_skip_install_dependencies_with_cli() {
-  (
-    cd $LOCAL_REPO
+test_skip_install_dependencies_with_cli() {(
+  cd $LOCAL_REPO
 
-    $GITUP -sa
-    assertEquals 0 $?
-    assertEquals 1 $mock_git_update_called
-    assertEquals 0 $mock_install_dependencies_called
-    assertEquals 1 $mock_migrations_called
-  )
-}
+  $GITUP -sa
+  assertEquals 0 $?
+  assertEquals 1 $mock_git_update_called
+  assertEquals 0 $mock_install_dependencies_called
+  assertEquals 1 $mock_migrations_called
+)}
 
-test_skip_migrations_with_cli() {
-  (
-    cd $LOCAL_REPO
+test_skip_migrations_with_cli() {(
+  cd $LOCAL_REPO
 
-    $GITUP -sm
+  $GITUP -sm
 
-    assertEquals 0 $?
-    assertEquals 1 $mock_git_update_called
-    assertEquals 1 $mock_install_dependencies_called
-    assertEquals 0 $mock_migrations_called
-  )
-}
+  assertEquals 0 $?
+  assertEquals 1 $mock_git_update_called
+  assertEquals 1 $mock_install_dependencies_called
+  assertEquals 0 $mock_migrations_called
+)}
 
 # MOCK FUNCTIONS
 # --------------
@@ -120,32 +109,28 @@ oneTimeTearDown() {
   rm -rdf $LOCAL_REPO
 }
 
-__setup_remote_repo() {
-  (
-    mkdir -p $REMOTE_REPO
-    cd $REMOTE_REPO
-    git init --bare
-  )
-}
+__setup_remote_repo() {(
+  mkdir -p $REMOTE_REPO
+  cd $REMOTE_REPO
+  git init --bare
+)}
 
-__setup_local_repo() {
-  (
-    git clone $REMOTE_REPO $LOCAL_REPO
-    cd $LOCAL_REPO
+__setup_local_repo() {(
+  git clone $REMOTE_REPO $LOCAL_REPO
+  cd $LOCAL_REPO
 
-    git checkout -b $MAIN_BRANCH
-    echo .gituprc >> $LOCAL_REPO/.gitignore
-    touch 'main_sample'
-    git add .
-    git commit -m 'added sample commit'
-    git push origin $MAIN_BRANCH
+  git checkout -b $MAIN_BRANCH
+  echo .gituprc >> $LOCAL_REPO/.gitignore
+  touch 'main_sample'
+  git add .
+  git commit -m 'added sample commit'
+  git push origin $MAIN_BRANCH
 
-    git checkout -b $DEV_BRANCH
-    touch 'dev_sample'
-    git add .
-    git commit -m 'added development commit'
-    git push origin $DEV_BRANCH
-  )
-}
+  git checkout -b $DEV_BRANCH
+  touch 'dev_sample'
+  git add .
+  git commit -m 'added development commit'
+  git push origin $DEV_BRANCH
+)}
 
 source ./shunit2/shunit2
